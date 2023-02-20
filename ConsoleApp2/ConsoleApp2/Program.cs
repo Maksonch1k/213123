3bc098d204b08ac818f6de50b4ed0b1549c3ed10
Maksonch1k <123887520+Maksonch1k@users.noreply.github.com>
2023-02-20 11:23:33 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Задача 1
+            //Console.Write("n=");
+            //byte n= byte.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"array[{i}]=");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //for(int i=0; i<array.Length; i++)
+            //{ 
+            //    Console.WriteLine("$array[{i}]={array[i]}");
+
+            //Задача 2
+            //Console.Write("n=");
+            //byte n = byte.Parse(Console.ReadLine());
+            //int[] array = new int[n]; Random rnd = new Random();
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+
+            //    array[i] = rnd.Next(-20, 20);
+            //}
+            //foreach (int elem in array)
+            //{
+            //    Console.Write($"{elem}\t");
+
+            Console.Write("n=");
+            byte n = byte.Parse(Console.ReadLine());
+            int[] array = new int[];
+            int max = array[0]
+                for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                    }
+
+
+
+
+
+        }
+    }
+    }
+}
